0ae7eeab6848a2fa6520ab6640d5dbcb6adaaa4b
irem <iremfeyzademir@gmail.com>
2025-01-31 14:12:21 +0300

    Update ana.py

--- ana.py
+++ ana.py
@@ -1,1 +1,13 @@
+import veri_isle
+import grafik_ciz
+
+# Veriyi oku
+df = veri_isle.veri_oku("veri.csv")
+
+# Veriyi analiz et ve ekrana yazdır
+analiz_sonucu = veri_isle.veri_analiz_et(df)
+print(analiz_sonucu)
+
+# Grafik çiz
+grafik_ciz.grafik_ciz(df)
 
