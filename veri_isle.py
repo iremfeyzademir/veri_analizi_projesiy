b98295a5950939a81da952cad5e9bd92d8b2c32d
irem <iremfeyzademir@gmail.com>
2025-01-31 14:11:16 +0300

    Update veri_isle.py

--- veri_isle.py
+++ veri_isle.py
@@ -1,1 +1,15 @@
+import pandas as pd
+
+def veri_oku(dosya_adi):
+    """
+    CSV dosyasını okur ve Pandas DataFrame olarak döndürür.
+    """
+    df = pd.read_csv(dosya_adi)
+    return df
+
+def veri_analiz_et(df):
+    """
+    Verinin temel istatistiklerini döndürür.
+    """
+    return df.describe()
 
